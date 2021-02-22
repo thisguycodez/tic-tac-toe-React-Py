3030cb8a05ad4b13d16789967da8b60dc9494d12
tgc <codingviewdmv@gmail.com>
2021-02-22 11:18:17 -0500

    Updates

---
+++ precache-manifest.7c5303696599e70dd91ffd265e0b372e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56d121897fb292b84bda252bbe61ada",
+    "url": "/tic-tac-toe-React-Py/index.html"
+  },
+  {
+    "revision": "64cd89c6276091f1b581",
+    "url": "/tic-tac-toe-React-Py/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8a043f9fa2a5b2f7a789",
+    "url": "/tic-tac-toe-React-Py/static/css/main.8222b6f7.chunk.css"
+  },
+  {
+    "revision": "64cd89c6276091f1b581",
+    "url": "/tic-tac-toe-React-Py/static/js/2.704f3206.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/tic-tac-toe-React-Py/static/js/2.704f3206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a043f9fa2a5b2f7a789",
+    "url": "/tic-tac-toe-React-Py/static/js/main.dd9d8a64.chunk.js"
+  },
+  {
+    "revision": "abf1bef53a17b93d6ccf",
+    "url": "/tic-tac-toe-React-Py/static/js/runtime-main.7fd294c5.js"
+  },
+  {
+    "revision": "cd0f9188c854620efea93fa71a4c17a8",
+    "url": "/tic-tac-toe-React-Py/static/media/1.cd0f9188.png"
+  },
+  {
+    "revision": "6f9ebf4dc008896427cda0ab3719b401",
+    "url": "/tic-tac-toe-React-Py/static/media/bg.6f9ebf4d.jpg"
+  },
+  {
+    "revision": "57d520467b61d62a7d97ada2bd455d10",
+    "url": "/tic-tac-toe-React-Py/static/media/pc.57d52046.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fedcc4830351148db4f0e7928bbc153f.js"
+  "/tic-tac-toe-React-Py/precache-manifest.7c5303696599e70dd91ffd265e0b372e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-toe-React-Py/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
